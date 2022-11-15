b6aeebe489445019fc4d16a18228fd4e6d57ea7d
flavinha <flaviarlimasantos@gmail.com>
2022-11-15 16:39:40 -0300

    feat: add nav

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BookComponent } from './shared/animations/book/book.component';
+import { defineElement } from 'lord-icon-element';
+import lottie from 'lottie-web';
+import { DisabledComponent } from './shared/animations/disabled/disabled.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BookComponent,
+    DisabledComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor (){
+    defineElement(lottie.loadAnimation)
+  }
+}
